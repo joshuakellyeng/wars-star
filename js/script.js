e6a361af319813c84e9653961b0d4e3925d4a32c
Joshua Kelly <Kellyjoshua08@gmail.com>
2021-12-12 10:27:34 -0500

    When entered the combat phase I'm using functions to simplify combat and make code execution alot cleaner.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-class StarFighter { //This is the basis of my classes I will be building two subclasses the rebels and the empire based off of a fan favorite ---- WARS STAR ------ *queue the music* babababab deee deee deee do deedoo daaah dahh
+ 
+ 
+ //This is the basis of my classes I will be building two subclasses the rebels and the empire based off of a fan favorite ---- WARS STAR ------ *queue the music* babababab deee deee deee do deedoo daaah dahh. I think I had to much fun making this.
+
+
+
+
+
+ class StarFighter {
     constructor(pilot, hull, accuracy, blasters) {
        this.pilot = pilot
        this.hull = hull
@@ -6,6 +14,9 @@ class StarFighter { //This is the basis of my classes I will be building two sub
        this.blasters = blasters
     }
 }
+
+
+//=======================================================================EMPIRE STAR FIGHTER SUBCLASS
 // this will be the empire class style ships or out "Bow"-Tie Fighters XD
 class Empire extends StarFighter {
     constructor(pilot, hull, accuracy, blasters, emp) {
@@ -13,11 +24,32 @@ class Empire extends StarFighter {
         this.emp = emp
     }
     fireBlasters() {
-        
-        
+        xWing.hull -= this.blasters
+        if(xWing.hull >= 0) {
+            console.log(`Empire Pilot ${this.pilot}: Take that Rebel Scum!      
+            R2: *You've taken damage Commander ${xWing.pilot}, current hull strength is at ${xWing.hull}*`)
+        } else {
+            console.log(`Die Rebel Scum!`)
+        }
+    }
+    fireEmp() {
+        xWing.hull -= this.emp
+        if(xWing.hull > 0) {
+            console.log(`Empire Pilot ${this.pilot}: I'll wipe you off the face of the Galaxy! Rebel Trash! R2:${xWing.hull} can't take anymore damage!`)
+        } else {
+            console.log(`Woohoooo! We blasted 'em!`)
+        }
+    }
+    retreat() {
+        console.log('WE WILL RETURN WITH THE MIGHT OF THE EMPIRE REBEL SCUM!')
+    }
+    death() {
+        console.log(`Empire Pilot ${bowTieFighter.pilot} : GAAAAAAAAAAAAAAAAAAAAAHHHHH!----------disconnected-----------`)
     }
 }
 
+
+//=======================================================================REBEL STAR FIGHTER SUBCLASS
 
 // this will be our ever resilient Rebel Squadron, I added features to make it scarier 
 class Rebel extends StarFighter {
@@ -26,34 +58,97 @@ class Rebel extends StarFighter {
         this.protonMissiles = protonMissiles
     } //pew pew pew 
     fireBlasters() {
+        bowTieFighter.hull -= this.blasters
         if(bowTieFighter.hull >= 0) {
-            console.log(`R2: *Enemy's hull is down to : ${bowTieFighter.hull -= this.blasters}*`)
+            console.log(`R2: *Enemy's hull is down to : ${bowTieFighter.hull}*`)
         } else {
-            console.log(`Woohoooo! Good Work R2!`)
+            console.log(bowTieFighter.death())
+            console.log(`KABOOM! Woohoooo! Good Work R2!`)
         }
     }
     //this is a fire proton missiles method
     fireProtonMissiles() {
-        
+        bowTieFighter.hull -= this.protonMissiles
         if(bowTieFighter.hull > 0) {
-            console.log(`Enemy's hull is down to : ${bowTieFighter.hull -= this.protonMissiles}`)
+            console.log(`R2: *Enemy's hull is down to : ${bowTieFighter.hull}*`)
         } else {
-            console.log(`Woohoooo! We blasted 'em!`)
+            console.log(`Commander${this.pilot}: Woohoooo! We blasted 'em!`)
         }
     }
     retreat() {
-        console.log('I think its time we skidaddle R2!')
+        console.log(`Commander${this.pilot}: I think its time we skidaddle R2!`)
     }
     death() {
-        console.log('Master Kenobi tell Padme I lo-*static noises* beep beep beeep ----------disconnected-----------')
+        console.log(`Commander${this.pilot}: Master Kenobi tell Padme I lo-*static noises* beep beep beeep ----------disconnected-----------`)
     }
 }
+
+
+//=======================================================================CAST OF PLAYERS AND ENEMIES
+
 const bowTieFighter = new Empire('Darth Siddious', 6, .8, 4, 15)
 
 const xWing = new Rebel('Anakin Planeswalker', 20, .7, 5, 8)
 
+//=======================================================================REBELS TURN FUNCTION
 
-xWing.fireBlasters()
+
+playerTurn = () => {
+
+    let playersChoice = prompt(`We've got # of bogies in the sky what should we do R2?!  Your choices are 'fire lasers', 'fire missiles', or 'retreat'`)
+
+
+    while (playersChoice !== 'firelaser' || 'fire missiles' || 'retreat') {
+        playersChoice = prompt(`Please enter a valid command: Your choices are 'fire lasers', 'fire missiles', or 'retreat'`)
+    }
+   
+    if(playersChoice.toLowerCase() === 'fire lasers') {
+        console.log(playersChoice)
+        console.log(xWing.fireBlasters())
+    } else if (playersChoice.toLowerCase() === 'fire missiles') {
+        console.log(playersChoice)
+        console.log(xWing.fireProtonMissiles())
+    } else if (playersChoice.toLowerCase() === 'retreat') {
+        console.log(playersChoice)
+        console.log(xWing.retreat())
+    } 
+
+//=======================================================================EMPIRE TURN FUNCTION
+
+
+empireTurn = () => {
+    console.log(bowTieFighter.fireBlasters())
+}
+
+
+
+
+
+
+//=========================================================== AND NOW WE SET THE TEXT/TURN/TERMINAL BASED COMBAT 
+
+dogFight = () => {
+ xWing.pilot = prompt("Thank you for volunteering to be a guardian of Tatooine young Commander! What may we call you?")
+ console.log(`Well we appreciate your service Commander ${xWing.pilot}, I apologize for the brief introductions but, we've received reports Empire troops extorting citizens entering our skies. Look! There's one now!`)
+ 
+ while(bowTieFighter.hull > 0) {
+    playerTurn();
+    empireTurn();
+ }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+dogFight()
 
 
 
