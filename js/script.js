9d75b30541675128717fd8e68cfa0066f6231aeb
Joshua Kelly <Kellyjoshua08@gmail.com>
2021-12-15 23:35:16 -0500

    I have a functioning 1v1 game

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ const playerAccuracy = document.querySelector('#player-accuracy');
 
 //turnIndicator
 const turnIndicator = document.querySelector('#whose-turn')
+const enemyTurn = document.querySelector('#enemy-turn')
 
 //class========================================
 class Spaceship {
@@ -24,39 +25,88 @@ class Spaceship {
 		this.firepower = firepower;
 		this.accuracy = accuracy;
 	}
-	attack() {}
+	attack() {
+		let randomNum = Math.random()
+		if(randomNum < this.accuracy){
+			alienShip.hull -= this.firepower
+			enemyHull.innerHTML = alienShip.hull
+			turnIndicator.innerHTML = `Nice shot! Aim was ${randomNum}, you dealt ${this.firepower} damage!`
+		} else {
+			turnIndicator.innerHTML = `Oh no you missed!`
+		}
+	}
 }
+
+class AlienShip {
+	constructor(hull, firepower, accuracy) {
+		this.hull = hull;
+		this.firepower = firepower;
+		this.accuracy = accuracy;
+	}
+	attack() {
+		let randomNum = Math.random()
+		if(randomNum < this.accuracy){
+			playerShip.hull -= this.firepower
+			playerHull.innerHTML = playerShip.hull
+			enemyTurn.innerHTML = `Oh No! Aliens aim was ${randomNum}, they dealt ${this.firepower} damage!`
+		} else {
+			enemyTurn.innerHTML = `Yes! Alien missed!`
+		}
+	}
+}
+
+
 
 const playerShip = new Spaceship(20, 5, 0.7);
 
-const squadron = [];
-
-for (let i = 0; i < 6; i++) {
-	squadron[i] = new Spaceship(
+const alienShip = new AlienShip(
 		Math.floor(Math.random() * 4) + 3,
 		Math.floor(Math.random() * 3) + 2,
 		(Math.floor(Math.random() * 3) + 6) / 10
 	);
+
+
+const battle = () => {
+	playerShip.attack()
+	if(alienShip.hull > 0) {
+		alienShip.attack()
+		// turnIndicator.innerHTML = 'If you wish to continue click "Attack", If you wish to flee click "Retreat"!'
+	} else {
+		turnIndicator.innerHTML = 'YOU WIN!'
+		// location.reload()
+	}
+
 }
-
-
 
 const game = {
 	start: () => {
 		startButton.classList.add('hide');
 		gameWindow.classList.remove('hide');
 		titleCard.classList.add('hide')
+
+
+		//alienShip
+		enemyHull.innerHTML = alienShip.hull
+    	enemyFirepower.innerHTML = alienShip.firepower
+    	enemyAccuracy.innerHTML = alienShip.accuracy
+		
+		
+		//UI
+		playerHull.innerHTML = playerShip.hull
+		playerAccuracy.innerHTML = playerShip.accuracy
+		playerFirepower.innerHTML = playerShip.firepower
+
 	},	
+
 	retreat: () => {
 		gameWindow.classList.add('hide')
 		startButton.classList.remove('hide')
 		startButton.innerText = 'RESTART'
+		location.reload()
 	},
 
 };
 
-console.log(playerShip)
-console.log(squadron)
 
 
 
